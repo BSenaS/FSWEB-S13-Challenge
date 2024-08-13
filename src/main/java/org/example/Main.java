873af6cd3db165f1d5f6310c34509d8b0a17d4b0
Batuhan Sena Sert <batuhansenasert@gmail.com>
2024-08-13 15:56:14 +0300

    Dependency graph enabled.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,27 +5,18 @@ public class Main {
     public static void main(String[] args) {
 
         printResults();
-
     }
-
     public static void printResults() {
 
         Employee employee = new Employee(1, "John Doe", "john@example.com", "password123", new String[]{"Plan1", "Plan2"});
 
         System.out.println(employee.toString());
 
-
-
-
         Company company = new Company(1, "ABC Company", 10000, new String[]{"Developer1", "Developer2"});
 
         System.out.println(company.toString());
 
-
-
-
         Healthplan healthPlan = new Healthplan(1, "My Health Plan", Plan.BASIC);
-
         System.out.println(healthPlan.toString());
 
     }
